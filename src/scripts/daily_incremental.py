03eb1dfd3a6e61f1cace4c831207829835b4d2b5
rachardv <38317150+rachardv@users.noreply.github.com>
2025-12-18 20:24:28 -0800

    pipeline v1

--- src/scripts/daily_incremental.py
+++ src/scripts/daily_incremental.py
@@ -1,13 +1,21 @@
 import extract, transform, load, config
+import logging
+logger = logging.getLogger(__name__)
+import sys
 
 
 def main():
     env_config = config.load_config()
 
-    cuffoff_date = load._get_latest_recorded_date(env_config)
+    cutoff_date = load._get_latest_recorded_date(env_config)
 
     # 1. Fetch raw data from site (cutoff_date=None)
-    raw_post_json = extract.run(env_config, cutoff_date=cuffoff_date)
+    raw_post_json = extract.run(env_config, cutoff_date=cutoff_date)
+
+    if len(raw_post_json) == 0:
+        logging.error(f"ERROR: No post found after cuttoff_date:{cutoff_date}")
+        sys.exit(1)
+
 
     # 2. Transform unstructured data to structured df
     clean_df = transform.run(env_config,raw_post_json)
